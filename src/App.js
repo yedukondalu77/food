79f1b8f8ea2c89699c14c99063e9e945565d41a6
yedukondalu77 <yedukondalunadakuditi21@gmail.com>
2024-12-30 17:32:54 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import FoodRecipe from "./Components/FoodRecipe";
-import ItemCard from "./Components/ItemCard";
+
 function App() {
   return (
     <div className="App" style={{backgroundColor:'#EBEDF4'}}>
       <BrowserRouter>
       <Routes>
         <Route path="/" element={<FoodRecipe/>}/>
-        <Route path="/item" element={<ItemCard/>}/>
+      
 
       </Routes>
       </BrowserRouter>
